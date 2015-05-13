e5609ff823070a484b19dbc722129cfbc0b9ed26
shahedcse <shahedcse.nu@gmail.com>
2015-05-13 12:51:21 +0600

    starting with github

---
+++ EmployeeAppThirteen/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace EmployeeAppThirteen
+{
+    partial class EmployeeAppUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.searchNameTextBox = new System.Windows.Forms.TextBox();
+            this.EmployeeListView = new System.Windows.Forms.ListView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(589, 72);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 22;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(413, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Search By Name";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Salary";
+            this.columnHeader5.Width = 97;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            this.columnHeader4.Width = 148;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.Width = 206;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 172;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // searchNameTextBox
+            // 
+            this.searchNameTextBox.Location = new System.Drawing.Point(530, 38);
+            this.searchNameTextBox.Name = "searchNameTextBox";
+            this.searchNameTextBox.Size = new System.Drawing.Size(135, 20);
+            this.searchNameTextBox.TabIndex = 21;
+            // 
+            // EmployeeListView
+            // 
+            this.EmployeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.EmployeeListView.FullRowSelect = true;
+            this.EmployeeListView.GridLines = true;
+            this.EmployeeListView.Location = new System.Drawing.Point(12, 238);
+            this.EmployeeListView.Name = "EmployeeListView";
+            this.EmployeeListView.Size = new System.Drawing.Size(713, 245);
+            this.EmployeeListView.TabIndex = 19;
+            this.EmployeeListView.UseCompatibleStateImageBehavior = false;
+            this.EmployeeListView.View = System.Windows.Forms.View.Details;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(260, 199);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 18;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(169, 97);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(167, 70);
+            this.addressTextBox.TabIndex = 16;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(168, 173);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(167, 20);
+            this.salaryTextBox.TabIndex = 17;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(169, 71);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(166, 20);
+            this.emailTextBox.TabIndex = 15;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(169, 45);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(166, 20);
+            this.nameTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Employee Entry";
+            // 
+            // EmployeeAppUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 485);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.searchNameTextBox);
+            this.Controls.Add(this.EmployeeListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EmployeeAppUI";
+            this.Text = "Employee App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox searchNameTextBox;
+        private System.Windows.Forms.ListView EmployeeListView;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EmployeeAppThirteen/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeAppThirteen
+{
+    public partial class EmployeeAppUI : Form
+    {
+        public EmployeeAppUI()
+        {
+            InitializeComponent();
+        }
+        EmployeeApp aeEmployeeApp = new EmployeeApp();
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            aeEmployeeApp.name = nameTextBox.Text;
+            aeEmployeeApp.email = emailTextBox.Text;
+            aeEmployeeApp.address = addressTextBox.Text;
+            aeEmployeeApp.salary = Convert.ToDouble(salaryTextBox.Text);
+
+
+            if (IsEmailExist(aeEmployeeApp.email))
+            {
+                MessageBox.Show("Email Already Exist");
+                return;
+            }
+
+            String connectionString =
+                @"SERVER= PC-301-22\SQLEXPRESS; DATABASE= EmployeeInfoDB; Integrated Security = true";
+            SqlConnection connection =new SqlConnection(connectionString);
+
+
+            String query = "INSERT INTO EmployeeT VALUES('" + aeEmployeeApp.name + "','" + aeEmployeeApp.address + "','" +
+                           aeEmployeeApp.email + "','" + aeEmployeeApp.salary + "')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            int rowAffected= command.ExecuteNonQuery();
+            connection.Close();
+
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Inserted Successfully!");
+
+            }
+            else
+            {
+                MessageBox.Show("Insertion Failed");
+            }
+        }
+
+        public bool IsEmailExist(string email)
+        {
+
+            
+
+            String connectionString =
+               @"SERVER= PC-301-22\SQLEXPRESS; DATABASE= EmployeeInfoDB; Integrated Security = true";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+
+            String query = "SELECT * FROM EmployeeT WHERE Email ='"+aeEmployeeApp.email+"' ";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            bool isEmailExist = false;
+            while (reader.Read())
+            {
+                isEmailExist = true;
+                break;
+            }
+            reader.Close();
+            connection.Close();
+            return isEmailExist;
+
+        }
+    }
+}
